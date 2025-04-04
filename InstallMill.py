caba8067cd93c867e199992a6cfb304f69e723fb
unknown <skilbride@nvidia.com>
2025-04-03 18:30:07 -0700

    cleanup unneeded functions, cleanup blender asset file

--- InstallMill.py
+++ InstallMill.py
@@ -11,18 +11,6 @@ from huggingface_hub import snapshot_download
 from huggingface_hub.utils import are_progress_bars_disabled, disable_progress_bars, enable_progress_bars
 from huggingface_hub import whoami
 from elevate import elevate
-
-
-if os.path.isfile('env.env'):
-
-    with open('env.env', mode ='r')as file:
-        hf_t = file.readline()
-        os.environ['HF_TOKEN']=hf_t
-        user = whoami(token=hf_t)
-        print('env.env file found, HuggingFace token id: '+user['id']+ ' for user: ' +user['name']+ ' will be utilized')
-else:
-    print('No env.env file was found. HuggingFace access token is not available. HuggingFace installations requiring HF validation will fail')
-              
         
 def is_admin():
     try:
@@ -119,68 +107,6 @@ class ManifestItem:
             return 'UNDEFINED'
 
 
-def is_long_path_enabled():
-    try:
-        # Open the registry key
-        registry_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SYSTEM\CurrentControlSet\Control\FileSystem")
-
-        # Query the 'LongPathsEnabled' value
-        long_paths_enabled, regtype = winreg.QueryValueEx(registry_key, "LongPathsEnabled")
-
-        # Close the registry key after query
-        winreg.CloseKey(registry_key)
-
-        # Check if the value is 1 (enabled)
-        if long_paths_enabled == 1:
-            return True
-        else:
-            return False
-
-    except FileNotFoundError:
-        # The registry key or value does not exist, meaning long path support is disabled
-        return False
-
-def enable_long_paths():
-    try:
-        # Open the registry key with write access
-        registry_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SYSTEM\CurrentControlSet\Control\FileSystem", 0, winreg.KEY_SET_VALUE)
-
-        # Set the 'LongPathsEnabled' value to 1 (to enable long path support)
-        winreg.SetValueEx(registry_key, "LongPathsEnabled", 0, winreg.REG_DWORD, 1)
-
-        # Close the registry key after setting the value
-        winreg.CloseKey(registry_key)
-
-        print("Successfully enabled long path support.")
-    except PermissionError:
-        print("Error: Administrator privileges are required to modify this registry key.")
-    except Exception as e:
-        print(f"Failed to enable long path support: {e}")
-
-def create_folder_junction(sourceFolderPath,destFolderPath):
-    if os.path.islink(destFolderPath):
-        print(f'Path {destFolderPath} is a symbolic link, attempting to unlink')
-        try:
-            os.unlink(destFolderPath)
-            print(f'{destFolderPath} has been unlinked')
-        except Exception as e:
-            print(f'Error attempting to unlink {destFolderPath}: {e}')
-    else:   
-        try:
-            shutil.rmtree(destFolderPath)
-        
-        except Exception as e:
-            print(f'Error deleting directory {destFolderPath}: {e}')
- 
-    try:
-            # Try to create a symbolic link (requires admin privileges or developer mode)
-            os.symlink(sourceFolderPath, destFolderPath,True)
-            print(f"Symbolic link created on Windows from {sourceFolderPath} to {destFolderPath}")
-    except OSError as e:
-            print(f"Failed to create symbolic link, falling back to junction. Error: {e}")
-            # Use mklink to create a junction instead
-            subprocess.check_call(['cmd', '/c', 'mklink', '/d', os.path.normpath(destFolderPath), os.path.normpath(sourceFolderPath)])
-            print(f"Folder junction created on Windows from {sourceFolderPath} to {destFolderPath}")
 
 def is_package_installed(package_name):
     """Check if the Python package is installed."""
@@ -365,138 +291,20 @@ def get_and_extract(url, location, overwrite, basefolder=os.getcwd()):
                     else:
                         stream_dl(url,location)
 
-#Check if Long Path Support is enabled
-if is_long_path_enabled():
-    print("Long Path Name support is enabled.")
-else:
-    print("#############################################################################")
-    print("Windows Long Path Name support is not enabled.")
-    print("Model downloads may fail if Long Path Name support is not enabled.")
-    print("#############################################################################")
-
-    validResponse = False
-
-    while not validResponse:
-        userInput = input('Would you like to enable Windows Long Path Name Support ? [y]|[n]')
-        if userInput.lower()=='y':
-            validResponse=True
-            if is_admin():
-                enable_long_paths()
-            else:
-                print("Administrator privileges are required...elevation will be requested.")
-                elevate()
-                enable_long_paths()
-            print('System reboot is required to enable Long Path Support...please reboot system and rerun setup using Option #3')
-            input("Press any key...reboot system before continuing installation")
-            sys.exit()
-        elif userInput.lower()=='n':
-            validResponse = True
-            print('Proceeding without support for Long Paths, model installations may fail...')
-            input('Press any key to continue the install without Long Path support...')
-        else:
-            print('Invalid option...')
-
-def create_directory_link(modelFolder, dirs: list[str]):
-    # Check if the modelFolder exists before proceeding
-    if not os.path.exists(modelFolder):
-        print(f"Model folder '{modelFolder}' does not exist. Operation aborted.")
-        return
-
-    for dir in dirs:
-        link_source = os.path.join(modelFolder, dir)
-        link_target = './ComfyUI/models/' + dir
-        
-        # Check if the source directory exists
-        if not os.path.exists(link_source):
-            print(f'Source directory {link_source} was not found. Skipping...')
-            continue
-
-        # Check if the target directory exists
-        if os.path.exists(link_target):
-            # Check if the target directory contains files
-            if len(os.listdir(link_target)) > 1:
-                print("Existing Target Directory contains files, model merge aborted... target directory must be empty.")
-                print(f"Target Directory: {link_target}")
-                continue  # Skip to the next directory if target is not empty
-        else:
-            # Create the target directory if it does not exist
-            try:
-                os.makedirs(link_target)
-                print(f"Target directory created: {link_target}")
-            except Exception as e:
-                print(f"Failed to create target directory '{link_target}': {e}")
-                traceback.print_exc()
-                continue
-
-        # Attempt to create the folder junction
-        try:
-            create_folder_junction(link_source, link_target)
-        except Exception as e:
-            print(f"An exception occurred while creating the folder junction: {e}")
-            traceback.print_exc()
-            print(f'Current Working Directory: {os.getcwd()}')
-            print(f'Attempted Link Source: {link_source}')
-            print(f'Attempted Link Target: {link_target}')
-        else:
-            print(f'Link Target "{link_target}" was successfully created from "{link_source}".')
-
-# Initialize the parser
-parser = argparse.ArgumentParser(description="Process some arguments.")
-
-# Add arguments
-parser.add_argument('-c',"--customManifest", type=str, help="Manifest File", default='./install_files.csv')
-parser.add_argument('-m',"--modelFolder", type=str, help="Model Folder")
-parser.add_argument('-i',"--installFolder", type=str, help="Install Folder", default=os.path.normpath(os.path.join(os.getcwd(),'ComfyUI_windows_portable')))
-parser.add_argument('-b',"--baseFolder", type=str, help="Base Folder", default=os.path.normpath(os.getcwd()))
-parser.add_argument('-u',"--update", action="store_true", help="Update ComfyUI")
-parser.add_argument('-a',"--allmodels", action="store_true", help="Copy All Models from source")
-
-
-
-# Parse the arguments
-args = parser.parse_args()
 
 #If the custom CSV file is a relative filepath, convert it to absolute.
 #This file pathpath must be set BEFORE changing the working directory.
-manifestFile = args.customManifest
+manifestFile = './install_files.csv'
 if not os.path.isabs(manifestFile):
     manifestFile=os.path.abspath(manifestFile)
 
 #Change the working directory to the install folder directory
-installFolder = args.installFolder
+installFolder = os.path.normpath(os.path.join(os.getcwd(),'ComfyUI_windows_portable'))
 print('Change the working folder to: '+installFolder)
 os.chdir(installFolder)
 
-#If the update flag is set, update ComfyUI to the latest version
-updateComfyUI = args.update
-if updateComfyUI:
-    updateDirectory = os.path.join(os.getcwd(),'update')
-    currentWorkingDir = os.getcwd()
-
-    subprocess.run([sys.executable, 'update.py', '../ComfyUI/'], cwd=updateDirectory)
-    if os.path.isfile(os.path.join(updateDirectory,'update_new.py')):
-        shutil.move(os.path.join(updateDirectory,'update_new.py'),os.path.join(updateDirectory,'update.py'))
-        subprocess.run([sys.executable, 'update.py', '../ComfyUI/', '--skip_self_update'], cwd=updateDirectory)
     
-baseFolder = args.baseFolder
-allModels = args.allmodels
-
-#If a modelFolder is supplied create the symlinks for the directories in the new installation.
-modelFolder = args.modelFolder
-if modelFolder is not None:
-    if not is_admin():
-        print('Using the --modelFolder -m requires administrator rights, please run from an Elevated Command Prompt')
-        sys.exit()
-    else:
-        elevate()
-        if allModels:
-            directories = [name for name in os.listdir(modelFolder) 
-                if os.path.isdir(os.path.join(modelFolder, name))]
-            print(directories)
-            create_directory_link(modelFolder, directories)
-        else:
-            create_directory_link(modelFolder, ['checkpoints','clip','loras','unet','vae'])
-
+baseFolder = os.path.normpath(os.getcwd())
 
 # Now get the ComfyManager from it's git repo
 repository_url = "https://github.com/ltdrdata/ComfyUI-Manager.git"
--- Setup.bat
+++ Setup.bat
@@ -5,15 +5,9 @@ set "base_dir=%cd%"
 set "comfyui_install_dir=.\ComfyUI_windows_portable\"
 
 
-REM Install 7zip
-REM winget install -e --id 7zip.7zip
-
-REM Install GIT 
-REM winget install -e --id Git.Git
-
 REM if no arguments are present or we have searched all arguments without finding one we want to act on, go to the comfyui install step
 IF "%~1"=="" (
-    ECHO NO ARGUMENTS SUPPLIED
+    
     GOTO CheckExistingComfyInstall
 )
 
@@ -62,14 +56,14 @@ SET "customManifest=True"
 :CheckExistingComfyInstall
 REM Check to see if this is a default or previous user install with an existing ComfyUI installation
 IF EXIST %comfyui_install_dir%comfyui (
-    ECHO An existing ComfyUI installation was found at: %comfyui_install_dir%
+    
     GOTO GetUserInput
 )
 
 REM Check to see if this is a custom location install with an existing ComfyUI installation
 IF EXIST %comfyui_install_dir%ComfyUI_windows_portable\comfyui (
     SET "comfyui_install_dir=%comfyui_install_dir%ComfyUI_windows_portable\"
-    ECHO An existing ComfyUI installation was found at: %comfyui_install_dir%
+    
     GOTO GetUserInput
 )
 
@@ -80,31 +74,14 @@ GOTO StartInstall
 :GetUserInput
 REM Prompt the user to decide how to proceed
 ECHO An existing ComfyUI installation was found in this directory. How would you like to proceed?
-echo 1. Download fresh ComfyUI, overwrite existing installation, and install component files
-ECHO 2. Update ComfyUI to current and install component files
-ECHO 3. Only install component files  - (choose this when resuming an installation)
-ECHO 4. Open existing ComfyUI without making changes
+ECHO 1. Resume Installation 
 ECHO 0. Exit
     
-SET /P choice="Enter your choice (1,2,3 or 4): "
+SET /P choice="Enter your choice (1 or 0): "
 ECHO Option %choice% was selected
 IF "%choice%"=="1" (
-	ECHO Downloading and installing ComfyUI and installing component files
-	GOTO StartInstall
-) ELSE IF "%choice%"=="2" (
-	pushd %comfyui_install_dir%update
-	ECHO Updating existing ComfyUI and installing component files
-	CALL update_comfyui.bat
-	popd
-	GOTO InstallPythonPackages        
-) ELSE IF "%choice%"=="3" (
 	ECHO Installing component files only
 	GOTO InstallPythonPackages
-) ELSE IF "%choice%"=="4" (
-	pushd %comfyui_install_dir%
-	CALL run_nvidia_gpu.bat
-	popd
-	GOTO END
 ) ELSE IF "%choice%"=="0" (
 	GOTO END
 ) ELSE (
@@ -123,7 +100,6 @@ IF %ERRORLEVEL% NEQ 0 (
     ECHO Problem with file download, try again
     EXIT
 ) ELSE (
-    REM Since 7z is not normally in the path, it's hard coded to the default location
     ECHO Extract ComfyUI
     IF "%comfyui_install_dir%"==".\ComfyUI_windows_portable\" (
 	tar -xvf .\ComfyUI_windows_portable_nvidia_or_cpu_nightly_pytorch.7z
